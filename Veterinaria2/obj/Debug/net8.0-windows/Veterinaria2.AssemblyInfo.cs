94c24bb97db12f5efcbaeee6c7846a6a57608392
JuanRa <jraguilar012012@gmail.com>
2025-07-19 00:04:27 -0600

    Veterinarios
    Veterinarios

--- Veterinaria2/obj/Debug/net8.0-windows/Veterinaria2.AssemblyInfo.cs
+++ Veterinaria2/obj/Debug/net8.0-windows/Veterinaria2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e8ea7aa6822d330584d1357489041d23100660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821f5baf9b572f6c2f25ae839bc75341fdc317a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
