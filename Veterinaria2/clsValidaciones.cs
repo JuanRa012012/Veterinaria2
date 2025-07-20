d8d257435ab4a84226ce41fda89dbf559b8e2fe7
chris194aniel <chris194aniel@gmail.com>
2025-07-20 16:31:28 -0600

    Ignore
    Ignore

--- Veterinaria2/clsValidaciones.cs
+++ Veterinaria2/clsValidaciones.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,46 @@ namespace Veterinaria2
 {
     public class clsValidaciones
     {
+        string conexion = "Data Source = CHRISTINA-PC\\SQLEXPRESS; Initial Catalog = BDVeterinaria; Integrated Security = true";
+
+        /// <summary>
+        /// Valida las credenciales del usuario.
+        /// </summary>
+        /// <param name="usuario">Nombre de usuario</param>
+        /// <param name="contraseña">Contraseña</param>
+        /// <param name="rol">Rol obtenido si las credenciales son válidas</param>
+        /// <returns>True si las credenciales son válidas y el estado es Activo</returns>
+        /// 
+
+        public bool ValidarUsuario(string usuario, string contraseña, out string rol)
+        {
+            rol = "";
+
+            using (SqlConnection conn = new SqlConnection(conexion))
+            {
+                conn.Open();
+                string query = "SELECT Rol FROM UsuarioSistema WHERE NombreUsuario = @Nombre AND Contraseña = @Contraseña AND Estado = 'Activo'";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", usuario);
+                    cmd.Parameters.AddWithValue("@Contraseña", contraseña);
+
+                    var resultado = cmd.ExecuteScalar();
+
+                    if (resultado != null)
+                    {
+                        rol = resultado.ToString();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
         public void SoloEnteros(KeyPressEventArgs e)
         {
             if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
@@ -31,5 +72,6 @@ namespace Veterinaria2
             else
                 e.Handled = false;
         }
+
     }
 }
