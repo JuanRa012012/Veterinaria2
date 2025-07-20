c07b5aacb14048481efb751c4969516e6a3eee73
JuanRa <jraguilar012012@gmail.com>
2025-07-19 22:28:34 -0600

    Razas

--- Veterinaria2/clsRazasConexion.cs
+++ Veterinaria2/clsRazasConexion.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Veterinaria2
 {
     internal class clsRazasConexion
     {
+        clsConexion clsConexion = new clsConexion();
+
+        SqlDataAdapter da; 
+
+        DataTable dt;
+
+        public void CargarDatos(DataGridView dgv)
+        {
+            try
+            {
+                da = new SqlDataAdapter("SELECT ID, DESCRIPCION FROM RAZAS WHERE ESTADO = 1", clsConexion.sc);
+                dt = new DataTable();
+                da.Fill(dt);
+
+
+            }
+        }
+
     }
 }
