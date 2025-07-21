212d132e65df991cc529bdcdefa2a31afb4857c3
chris194aniel <chris194aniel@gmail.com>
2025-07-20 21:08:36 -0600

    cambios

--- Veterinaria2/clsValidaciones.cs
+++ Veterinaria2/clsValidaciones.cs
@@ -9,45 +9,6 @@ namespace Veterinaria2
 {
     public class clsValidaciones
     {
-        string conexion = "Data Source = CHRISTINA-PC\\SQLEXPRESS; Initial Catalog = BDVeterinaria; Integrated Security = true";
-
-        /// <summary>
-        /// Valida las credenciales del usuario.
-        /// </summary>
-        /// <param name="usuario">Nombre de usuario</param>
-        /// <param name="contraseña">Contraseña</param>
-        /// <param name="rol">Rol obtenido si las credenciales son válidas</param>
-        /// <returns>True si las credenciales son válidas y el estado es Activo</returns>
-        /// 
-
-        public bool ValidarUsuario(string usuario, string contraseña, out string rol)
-        {
-            rol = "";
-
-            using (SqlConnection conn = new SqlConnection(conexion))
-            {
-                conn.Open();
-                string query = "SELECT Rol FROM UsuarioSistema WHERE NombreUsuario = @Nombre AND Contraseña = @Contraseña AND Estado = 'Activo'";
-
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Nombre", usuario);
-                    cmd.Parameters.AddWithValue("@Contraseña", contraseña);
-
-                    var resultado = cmd.ExecuteScalar();
-
-                    if (resultado != null)
-                    {
-                        rol = resultado.ToString();
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
         public void SoloEnteros(KeyPressEventArgs e)
         {
             if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
--- Veterinaria2/obj/Debug/net8.0-windows/Veterinaria2.AssemblyInfo.cs
+++ Veterinaria2/obj/Debug/net8.0-windows/Veterinaria2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06547cef21b6a09ccd2f59aa833586dd2e5d20cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9b436ccc41fba732a0f7d8f54de01be58d8403c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
