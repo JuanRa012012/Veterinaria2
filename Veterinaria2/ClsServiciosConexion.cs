de9795ca7c9345bbbefa044d857dd1562986d110
pablohimself19 <pablolagos2004andre@gmail.com>
2025-07-20 19:39:31 -0600

    Servicios

---
+++ Veterinaria2/ClsServiciosConexion.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria2
+{
+    internal class ClsServiciosConexion
+    {
+        clsConexion clsConexion = new clsConexion();
+        SqlDataAdapter da;
+        DataTable dt;
+        SqlCommand cmd;
+
+        public void CargarDatos(DataGridView dgv)
+        {
+            try
+            {
+                da = new SqlDataAdapter("SELECT ID, NOMBRESERVICIO, PRECIO FROM SERVICIO WHERE ESTADO = 1", clsConexion.sc);
+                dt = new DataTable();
+                da.Fill(dt);
+                dgv.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "State", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void BuscarServicio(DataGridView dgv, String vrBuscar)
+        {
+            try
+            {
+                da = new SqlDataAdapter("SELECT ID, NOMBRESERVICIO, PRECIO FROM SERVICIO WHERE NOMBRESERVICIO LIKE '%" + vrBuscar + "%' AND ESTADO = 1", clsConexion.sc);
+                dt = new DataTable();
+                da.Fill(dt);
+                dgv.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "State", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void InsertServicio(DataGridView dgv, String vrNombre, int vrPrecio)
+        {
+            try
+            {
+                clsConexion.Abrir();
+                cmd = new SqlCommand("INSERT INTO SERVICIO VALUES (@nombre, @precio, 1)", clsConexion.sc);
+                cmd.Parameters.AddWithValue("@nombre", vrNombre);
+                cmd.Parameters.AddWithValue("@precio", vrPrecio);
+                cmd.ExecuteNonQuery();
+                CargarDatos(dgv);
+                clsConexion.Abrir();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "State", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void UpdateServicio(DataGridView dgv, string vrNombre, decimal vrPrecio, int vrID)
+        {
+            try
+            {
+                clsConexion.Abrir();
+                cmd = new SqlCommand("UPDATE SERVICIO SET NOMBRESERVICIO = @nombre, PRECIO = @precio WHERE ID = @id", clsConexion.sc);
+                cmd.Parameters.AddWithValue("@nombre", vrNombre);
+                cmd.Parameters.AddWithValue("@precio", vrPrecio);
+                cmd.Parameters.AddWithValue("@id", vrID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("El ítem ha sido modificado correctamente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarDatos(dgv);
+                clsConexion.Abrir();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void DeleteServicio(DataGridView dgv, int vrID)
+        {
+            try
+            {
+                clsConexion.Abrir();
+                cmd = new SqlCommand("UPDATE SERVICIO SET ESTADO = 2 WHERE ID = @id", clsConexion.sc);
+                cmd.Parameters.AddWithValue("@id", vrID);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("El ítem ha sido anulado correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarDatos(dgv);
+                clsConexion.Abrir();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex, "State", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
--- Veterinaria2/Servicios.cs
+++ Veterinaria2/Servicios.cs
@@ -12,14 +12,90 @@ namespace Veterinaria2
 {
     public partial class Servicios : Form
     {
+        ClsServiciosConexion obj = new ClsServiciosConexion();
+        int idActual = 0;
+
         public Servicios()
         {
             InitializeComponent();
+            obj.CargarDatos(grdServicios);
         }
 
-        private void cmdSalir_Click(object sender, EventArgs e)
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtPrecio.Text != "")
+            {
+                decimal precio;
+                if (decimal.TryParse(txtPrecio.Text, out precio))
+                {
+                    obj.InsertServicio(grdServicios, txtNombre.Text, precio);
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un precio válido.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Complete todos los campos.");
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtPrecio.Text != "" && idActual != 0)
+            {
+                decimal precio;
+                if (decimal.TryParse(txtPrecio.Text, out precio))
+                {
+                    obj.UpdateServicio(grdServicios, txtNombre.Text, precio, idActual);
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un precio válido.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un servicio de la lista para modificar.");
+            }
+        }
+
+        private void btnAnular_Click(object sender, EventArgs e)
+        {
+            if (idActual != 0)
+            {
+                obj.DeleteServicio(grdServicios, idActual);
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un servicio de la lista para anular.");
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void dgvServicios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                idActual = Convert.ToInt32(grdServicios.Rows[e.RowIndex].Cells["ID"].Value);
+                txtNombre.Text = grdServicios.Rows[e.RowIndex].Cells["NOMBRESERVICIO"].Value.ToString();
+                txtPrecio.Text = grdServicios.Rows[e.RowIndex].Cells["PRECIO"].Value.ToString();
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            idActual = 0;
         }
     }
 }
